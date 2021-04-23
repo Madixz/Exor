4add5255e651b85482f4adae33f07f1d4ab3a132
Exor <80299854+Madixz@users.noreply.github.com>
2021-04-23 22:12:52 +0800

    Update animation.py

--- userbot/modules/animation.py
+++ userbot/modules/animation.py
@@ -1227,6 +1227,75 @@ async def _(event):
 
             await event.edit(animation_chars[i % 6])
 
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sinick1":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "`Sayang Pengen🥺` ",
+            " Udah Kebelet ",
+            " Pengen Ngewe ",
+            " Ayangg Ayokkk ",
+            " Ngeweee ",
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])
+
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sinick2":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "`Masih Teringat` ",
+            " Di Malam Itu Kita Bercumbu",
+            " Di Bawah Sinar Rembulan ",
+            " Engkau MencumbuiKu ",
+            " Dengan Sangat Ganas ",
+            " Kau Isap Kontol Ku ",
+            " Dan Ku Jilat Memek Mu ",
+            " Skip Nanti Lu Sange",
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])
+
+
+
 
 CMD_HELP.update(
     {
