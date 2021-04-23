2d206a742ec78ab70e2e88321b18bc765969278a
Exor <80299854+Madixz@users.noreply.github.com>
2021-04-23 18:34:54 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -100,9 +100,9 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/ManusiaRakitan/Kampang-Bot.git")
+    "https://github.com/Madixz/Exor.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Kampang")
+    "UPSTREAM_REPO_BRANCH", "Sinick")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
