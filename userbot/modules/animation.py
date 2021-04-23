962923e8c3b9347189bfacf664143ca9b557a86a
Madika <madika989@gmail.com>
2021-04-23 14:15:58 +0000

    pylint: auto fixes
    Signed-off-by: Madixz <madika989@gmail.com>

--- userbot/modules/animation.py
+++ userbot/modules/animation.py
@@ -1227,6 +1227,7 @@ async def _(event):
 
             await event.edit(animation_chars[i % 6])
 
+
 @bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
@@ -1257,7 +1258,6 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 103])
-
 
 
 @bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
@@ -1293,8 +1293,6 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 103])
-
-
 
 
 CMD_HELP.update(
