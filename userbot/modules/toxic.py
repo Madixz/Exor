2a1a82a2b06f6ef17bb7b7a34fafa0f7cb28c9f1
Madika <madika989@gmail.com>
2021-04-23 17:56:33 +0000

    pylint: auto fixes
    Signed-off-by: Madixz <madika989@gmail.com>

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -1,8 +1,7 @@
-from time import sleep
 from userbot import CMD_HELP, bot
-from userbot.events import register
 from telethon import events
 import asyncio
+
 
 @bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
