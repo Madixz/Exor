d614adca49077ec0e2606151aec1fb4c9341f562
Exor <80299854+Madixz@users.noreply.github.com>
2021-04-24 16:19:47 +0800

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -3,25 +3,34 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot module for getting information about the server. """
 
+""" Userbot module for System Stats commands """
 
 import asyncio
-from asyncio import create_subprocess_exec as asyncrunapp
-from asyncio.subprocess import PIPE as asyncPIPE
-from platform import python_version, uname
-from shutil import which
-from os import remove
-from telethon import version
-from telethon import __version__, version
 import platform
 import sys
 import time
+from asyncio import create_subprocess_exec as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
 from datetime import datetime
-import psutil
-from userbot import ALIVE_LOGO, ALIVE_NAME, BOT_VER, CMD_HELP, StartTime, bot
-from userbot.events import register
+from os import remove
+from platform import python_version, uname
+from shutil import which
 
+import psutil
+from telethon import __version__, version
+
+from userbot import (
+    ALIVE_LOGO,
+    ALIVE_NAME,
+    ALIVE_TEKS_CUSTOM,
+    BOT_VER,
+    CMD_HELP,
+    UPSTREAM_REPO_BRANCH,
+    StartTime,
+    bot,
+)
+from userbot.events import register
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
@@ -35,13 +44,11 @@ async def get_readable_time(seconds: int) -> str:
     count = 0
     up_time = ""
     time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
+    time_suffix_list = ["s", "m", "Jam", "Hari"]
 
     while count < 4:
         count += 1
-        remainder, result = divmod(
-            seconds, 60) if count < 3 else divmod(
-            seconds, 24)
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
         if seconds == 0 and remainder == 0:
             break
         time_list.append(int(result))
@@ -61,21 +68,19 @@ async def get_readable_time(seconds: int) -> str:
 @register(outgoing=True, pattern=r"^\.spc")
 async def psu(event):
     uname = platform.uname()
-    softw = "**System Information**\n"
-    softw += f"`System   : {uname.system}`\n"
-    softw += f"`Release  : {uname.release}`\n"
-    softw += f"`Version  : {uname.version}`\n"
-    softw += f"`Machine  : {uname.machine}`\n"
+    softw = "**Informasi Sistem**\n"
+    softw += f"`Sistem   : {uname.system}`\n"
+    softw += f"`Rilis    : {uname.release}`\n"
+    softw += f"`Versi    : {uname.version}`\n"
+    softw += f"`Mesin    : {uname.machine}`\n"
     # Boot Time
     boot_time_timestamp = psutil.boot_time()
     bt = datetime.fromtimestamp(boot_time_timestamp)
-    softw += f"`Boot Time: {bt.day}/{bt.month}/{bt.year}  {bt.hour}:{bt.minute}:{bt.second}`\n"
+    softw += f"`Waktu Hidup: {bt.day}/{bt.month}/{bt.year}  {bt.hour}:{bt.minute}:{bt.second}`\n"
     # CPU Cores
-    cpuu = "**CPU Info**\n"
-    cpuu += "`Physical cores   : " + \
-        str(psutil.cpu_count(logical=False)) + "`\n"
-    cpuu += "`Total cores      : " + \
-        str(psutil.cpu_count(logical=True)) + "`\n"
+    cpuu = "**Informasi CPU**\n"
+    cpuu += "`Physical cores   : " + str(psutil.cpu_count(logical=False)) + "`\n"
+    cpuu += "`Total cores      : " + str(psutil.cpu_count(logical=True)) + "`\n"
     # CPU frequencies
     cpufreq = psutil.cpu_freq()
     cpuu += f"`Max Frequency    : {cpufreq.max:.2f}Mhz`\n"
@@ -86,23 +91,23 @@ async def psu(event):
     for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
         cpuu += f"`Core {i}  : {percentage}%`\n"
     cpuu += "**Total CPU Usage**\n"
-    cpuu += f"`All Core: {psutil.cpu_percent()}%`\n"
+    cpuu += f"`Semua Core: {psutil.cpu_percent()}%`\n"
     # RAM Usage
     svmem = psutil.virtual_memory()
-    memm = "**Memory Usage**\n"
+    memm = "**Memori Digunakan**\n"
     memm += f"`Total     : {get_size(svmem.total)}`\n"
     memm += f"`Available : {get_size(svmem.available)}`\n"
     memm += f"`Used      : {get_size(svmem.used)}`\n"
     memm += f"`Percentage: {svmem.percent}%`\n"
     # Bandwidth Usage
-    bw = "**Bandwith Usage**\n"
-    bw += f"`Upload  : {get_size(psutil.net_io_counters().bytes_sent)}`\n"
+    bw = "**Bandwith Digunakan**\n"
+    bw += f"`Unggah  : {get_size(psutil.net_io_counters().bytes_sent)}`\n"
     bw += f"`Download: {get_size(psutil.net_io_counters().bytes_recv)}`\n"
     help_string = f"{str(softw)}\n"
     help_string += f"{str(cpuu)}\n"
     help_string += f"{str(memm)}\n"
     help_string += f"{str(bw)}\n"
-    help_string += "**Engine Info**\n"
+    help_string += "**Informasi Mesin**\n"
     help_string += f"`Python {sys.version}`\n"
     help_string += f"`Telethon {__version__}`"
     await event.edit(help_string)
@@ -128,12 +133,11 @@ async def sysdetails(sysd):
             )
 
             stdout, stderr = await fetch.communicate()
-            result = str(stdout.decode().strip()) + \
-                str(stderr.decode().strip())
+            result = str(stdout.decode().strip()) + str(stderr.decode().strip())
 
             await sysd.edit("`" + result + "`")
         except FileNotFoundError:
-            await sysd.edit("`Install neofetch first !!`")
+            await sysd.edit("`Terlebih dahulu Install neofetch!!`")
 
 
 @register(outgoing=True, pattern=r"^\.botver$")
@@ -164,18 +168,10 @@ async def bot_ver(event):
         revout = str(stdout.decode().strip()) + str(stderr.decode().strip())
 
         await event.edit(
-            "`╭►▻►▻►▻►▻►▻►◄◅◄◅◄◅◄◅◄◅╮\n "
-            "` 🌀Exor  Version: \n "
-            f"{verout}"
-            "` \n"
-            "   Revision: "
-            f"{revout}🇲🇨\n"
-            "╰►▻►▻►▻►▻►▻►◄◅◄◅◄◅◄◅◄◅╯ "
+            "✥ **Userbot Versi :** " f"{verout}" "\n**✥ **Revisi :** " f"{revout}"
         )
     else:
-        await event.edit(
-            "Shame that you don't have git, you're running - 'v1.beta.4' anyway!"
-        )
+        await event.edit("anda tidak memiliki git, Anda Menjalankan Bot - 'v1.beta.4'!")
 
 
 @register(outgoing=True, pattern=r"^\.pip(?: |$)(.*)")
@@ -184,7 +180,7 @@ async def pipcheck(pip):
         return
     pipmodule = pip.pattern_match.group(1)
     if pipmodule:
-        await pip.edit("`Searching . . .`")
+        await pip.edit("`Mencari...`")
         pipc = await asyncrunapp(
             "pip3",
             "search",
@@ -198,7 +194,7 @@ async def pipcheck(pip):
 
         if pipout:
             if len(pipout) > 4096:
-                await pip.edit("`Output too large, sending as file`")
+                await pip.edit("`Output Terlalu Besar, Dikirim Sebagai File`")
                 file = open("output.txt", "w+")
                 file.write(pipout)
                 file.close()
@@ -223,13 +219,92 @@ async def pipcheck(pip):
                 "`\n**Result: **\n`No Result Returned/False`"
             )
     else:
-        await pip.edit("`Use .help pip to see an example`")
+        await pip.edit("Gunakan `.help pip` Untuk Melihat Contoh")
+
+
+@register(outgoing=True, pattern=r"^\.(?:calive|con)\s?(.)?")
+async def amireallyalive(alive):
+    user = await bot.get_me()
+    uptime = await get_readable_time((time.time() - StartTime))
+    output = (
+        f" **┗┓ ✮ USERBOT {DEFAULTUSER} ✮ ┏┛** \n"
+        f"\n__**{ALIVE_TEKS_CUSTOM}**__\n"
+        f"**━━━━━━━━━━━━━━━**\n"
+        f"**✮ Master ✮** \n"
+        f" ➥ `{DEFAULTUSER}` \n"
+        f"**✮ Username ✮** \n"
+        f" ➥ `@{user.username}` \n"
+        f"┏━━━━━━━━━━━━━━━━\n"
+        f"┣✣ `Telethon : `Ver {version.__version__} \n"
+        f"┣✣ `Python   : `Ver {python_version()} \n"
+        f"┣✣ `Bot Ver  : `{BOT_VER} \n"
+        f"┣✣ `Modules  : `{len(modules)} \n"
+        f"┣✣ `Uptime   : `{uptime} \n"
+        f"┗━━━━━━━━━━━━━━━━"
+    )
+    if ALIVE_LOGO:
+        try:
+            logo = ALIVE_LOGO
+            await alive.delete()
+            msg = await bot.send_file(alive.chat_id, logo, caption=output)
+            await asyncio.sleep(200)
+            await msg.delete()
+        except BaseException:
+            await alive.edit(
+                output + "\n\n *`The provided logo is invalid."
+                "\nMake sure the link is directed to the logo picture`"
+            )
+            await asyncio.sleep(100)
+            await alive.delete()
+    else:
+        await alive.edit(output)
+        await asyncio.sleep(100)
+        await alive.delete()
+
+
+@register(outgoing=True, pattern=r"^\.(?:xalive|xon)\s?(.)?")
+async def amireallyalive(alive):
+    user = await bot.get_me()
+    uptime = await get_readable_time((time.time() - StartTime))
+    output = (
+        f"۝⩵►{DEFAULTUSER} USERBOT◄⩵۝\n \n"
+        f"╭━━━━━━━━━━━━━━━━━━━━━╮\n"
+        f"┣[•👤 `USER     :`{DEFAULTUSER}\n"
+        f"┣[ 👁‍🗨 `Username :`@{user.username}\n"
+        "`┣▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱`\n"
+        f"┣[•⚙️ `Telethon :`v {version.__version__} 🔥\n"
+        f"┣[•🐍 `Python   :`v {python_version()} 🔥\n"
+        f"┣[•💻 `Base on  :`{UPSTREAM_REPO_BRANCH}🔥\n"
+        f"┣[•🛠 `Version  :`{BOT_VER} 🔥\n"
+        f"┣[•🗃 `Modules  :`{len(modules)} Loaded🔥\n"
+        f"┣[•🕒 `Uptime   :`{uptime} 🔥\n"
+        f"╰━━━━━━━━━━━━━━━━━━━━━╯\n"
+        f" • MOD BY : `{DEFAULTUSER}`"
+    )
+    if ALIVE_LOGO:
+        try:
+            logo = ALIVE_LOGO
+            await alive.delete()
+            msg = await bot.send_file(alive.chat_id, logo, caption=output)
+            await asyncio.sleep(100)
+            await msg.delete()
+        except BaseException:
+            await alive.edit(
+                output + "\n\n *`The provided logo is invalid."
+                "\nMake sure the link is directed to the logo picture`"
+            )
+            await asyncio.sleep(100)
+            await alive.delete()
+    else:
+        await alive.edit(output)
+        await asyncio.sleep(100)
+        await alive.delete()
 
 
 @register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def amireallyalive(alive):
     user = await bot.get_me()
-    await get_readable_time((time.time() - StartTime))
+    uptime = await get_readable_time((time.time() - StartTime))
     output = (
         f"**♕ ┗┓ {DEFAULTUSER} USERBOT ┏┛ ♕** \n\n"
         f"┏━━━━━━━━━━━━━━ \n"
@@ -242,21 +317,22 @@ async def amireallyalive(alive):
         f"┣ ✥ `Modules  : `{len(modules)} Modules \n"
         f"┣ ✥ `Uptime   : `{uptime} \n"
         f"┗━━━━━━━━━━━━━━ \n"
-        f"❃ **Owner :** [Bot_Sinick](t.me/Bot_Sinick) \n"
+        f"❃ **Repo Userbot :** [Man-Userbot](https://github.com/mrismanaziz/Man-Userbot) \n"
+        f"❃ **Grup Userbot :** [Tekan Disini](https://t.me/sharinguserbot) \n"
+        f"❃ **Owner :** [Risman](t.me/mrismanaziz) \n"
         f"┗━━━━━━━━━━━━━━ \n"
     )
-
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
             await alive.delete()
             msg = await bot.send_file(alive.chat_id, logo, caption=output)
-            await asyncio.sleep(200)
+            await asyncio.sleep(800)
             await msg.delete()
         except BaseException:
             await alive.edit(
-                output + "\n\n *`The provided logo is invalid."
-                "\nMake sure the link is directed to the logo picture`"
+                output + "\n\n *`Logo Yang Disediakan Tidak Valid."
+                "\nPastikan Tautan Yang Anda Gunakan Valid`"
             )
             await asyncio.sleep(100)
             await alive.delete()
@@ -285,22 +361,23 @@ async def amireallyalivereset(ureset):
     await ureset.edit("`" "Successfully reset user for alive!" "`")
 
 
-CMD_HELP.update({
-    "system":
-    "`.sysd`\
-\nUsage: Shows system information using neofetch.\
-\n\n`.botver`\
-\nUsage: Shows the userbot version.\
-\n\n`.pip` <module(s)>\
-\nUsage: Does a search of pip modules(s).\
-\n\n`.start`\
-\nUsage: Type .start to see whether your bot is working or not.\
-\n\n`.aliveu` <text>\
-\nUsage: Changes the 'user' in alive to the text you want.\
-\n\n`.resetalive`\
-\nUsage: Resets the user to default.\
-\n\n`.db`\
-\nUsage:Shows database related info.\
-\n\n.`.spc`\
-\nUsage:Show system specification."
-})
+CMD_HELP.update(
+    {
+        "system": "**Plugin : **`system`.\
+        \n\n  •  **Syntax :** `.sysd`\
+        \n  •  **Function : **Menampilkan informasi sistem menggunakan neofetch\
+        \n\n\n  •  **Syntax :** `.botver`\
+        \n  •  **Function : **Menampilkan versi userbot\
+        \n\n  •  **Syntax :** `.pip` <module(s)>\
+        \n  •  **Function : **Melakukan pencarian modul pip\
+        \n\n  •  **Syntax :** `.alive` atau `.on`\
+        \n  •  **Function : **Ketik .alive untuk melihat apakah bot Anda berfungsi atau tidak.\
+        \n\n  •  **Syntax :** `.resetalive`\
+        \n  •  **Function : **Untuk Membanned Akun yang sedang online\
+        \n\n  •  **Syntax :** `.db`\
+        \n  •  **Function : **Menampilkan info terkait database.\
+        \n\n  •  **Syntax :** `.spc`\
+        \n  •  **Function : **Show system specification\
+    "
+    }
+)
