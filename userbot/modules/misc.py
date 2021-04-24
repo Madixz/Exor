526859ff42475fb260d7213f09cb6cd06256a274
Exor <80299854+Madixz@users.noreply.github.com>
2021-04-24 14:20:53 +0800

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "**╭┄┅┯┅┄──────┄┅┯┅┄╮**\n\n           [BOT_SINICK](https://github.com/ManusiaRakitan/Kampang-Bot)\n\n**╰┄┅┷┅┄──────┄┅┷┅┄╯**\n"
+        "**╭┄┅┯┅┄──────┄┅┯┅┄╮**\n\n               [BOT_SINICK](https://github.com/ManusiaRakitan/Kampang-Bot)\n\n**╰┄┅┷┅┄──────┄┅┷┅┄╯**\n"
     )
 
 
