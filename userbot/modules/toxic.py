9ac321ac898596e526c76290e58394d2f4d91ba5
Exor <80299854+Madixz@users.noreply.github.com>
2021-04-24 19:35:06 +0800

    Update toxic.py

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -69,6 +69,46 @@ async def _(event):
 
             await event.edit(animation_chars[i % 103])
 
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sinick3":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "`Teruntuk Kamu` ",
+            " Hay Cantik, Sudah Lama ",
+            " Perasaan Ini Ku SembunyiKan ",
+            " Tapi Sekarang Aku ",
+            " Tak Dapat Menampung Semuanya ",
+            " Semakin Aku Dia Semakin Aku Gelisah ",
+            " Hati Ini Menginginkan Dirimu ",
+            " Mau Kah Kamu Menjadi PendampingKu?",
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])
+
+
+
+
+
+
+
 CMD_HELP.update({
     "toxic":
     "`.sinick1`\
