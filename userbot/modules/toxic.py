10dbf3cef5129371c075f35b9389878c229b3293
Madika <madika989@gmail.com>
2021-04-24 11:36:05 +0000

    pylint: auto fixes
    Signed-off-by: Madixz <madika989@gmail.com>

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -69,6 +69,7 @@ async def _(event):
 
             await event.edit(animation_chars[i % 103])
 
+
 @bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
@@ -102,11 +103,6 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 103])
-
-
-
-
-
 
 
 CMD_HELP.update({
