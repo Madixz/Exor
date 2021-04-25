4f22cfa74fab77555984c35b59dd4b654bab3fe1
Exor <80299854+Madixz@users.noreply.github.com>
2021-04-25 21:43:40 +0800

    Update toxic.py

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -105,35 +105,6 @@ async def _(event):
             await event.edit(animation_chars[i % 103])
 
 
-@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-async def _(event):
-
-    if event.fwd_from:
-
-        return
-
-    animation_interval = 3
-
-    animation_ttl = range(0, 103)
-
-    input_str = event.pattern_match.group(1)
-
-    if input_str == "bocep":
-
-        await event.edit(input_str)
-
-        animation_chars = [
-            "`Ch Bokep` ",
-            " https://t.me/joinchat/VYezAmeu_YM5NTQ9 ",
-        ]
-
-        for i in animation_ttl:
-
-            await asyncio.sleep(animation_interval)
-
-            await event.edit(animation_chars[i % 103])
-
-
 CMD_HELP.update({
     "toxic":
     "`.sinick1`\
